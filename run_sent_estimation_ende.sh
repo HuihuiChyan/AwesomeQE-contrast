ef0aa7f54ecc210fa587ce0200fc356e98b5d065
huanghui.1997 <huanghui.1997@bytedance.com>
2023-05-31 11:53:37 +0800

    simple test

--- run_sent_estimation_ende.sh
+++ run_sent_estimation_ende.sh
@@ -1,28 +1,28 @@
 export CUDA_VISIBLE_DEVICES=1
 
 LABEL_SUFFIX=score
-# accelerate launch --fp16 run_quality_estimation.py \
-#  --do_train \
-#  --data_dir ./data/en-de/ \
-#  --model_type xlmr \
-#  --model_path ./xlm-roberta-base \
-#  --output_dir ./QE_outputs/en-de \
-#  --batch_size 8 \
-#  --learning_rate 1e-5 \
-#  --max_epoch 20 \
-#  --valid_steps 500 \
-#  --train_type sent \
-#  --valid_type sent \
-#  --sentlab_suffix $LABEL_SUFFIX \
-#  --stop_criterion 10 \
-#  --best_metric pearson \
-#  --pad_to_max_length \
-#  --max_length 200 \
-#  --contrast_time 20 \
-#  --overwrite_output_dir \
-#  --overwrite_cache \
-#  --do_contrast \
-#  --only_contrast
+accelerate launch --fp16 run_quality_estimation.py \
+ --do_train \
+ --data_dir ./data/en-de/ \
+ --model_type xlmr \
+ --model_path ./xlm-roberta-base \
+ --output_dir ./QE_outputs/en-de \
+ --batch_size 8 \
+ --learning_rate 1e-5 \
+ --max_epoch 20 \
+ --valid_steps 500 \
+ --train_type sent \
+ --valid_type sent \
+ --sentlab_suffix $LABEL_SUFFIX \
+ --stop_criterion 10 \
+ --best_metric pearson \
+ --pad_to_max_length \
+ --max_length 200 \
+ --contrast_time 20 \
+ --overwrite_output_dir \
+ --overwrite_cache \
+ --do_contrast \
+ --only_contrast
 
 INFER_PREFIX=dev
 python run_quality_estimation.py \
